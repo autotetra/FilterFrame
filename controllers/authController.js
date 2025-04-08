b1864a14918edfb875c4aa2351930f8a74dcf37b
Christos Stylidis <christos@autotetra.com>
2025-04-08 03:49:30 +0300

    setup auth

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,7 @@
 const bcrypt = require("bcrypt");
 const User = require("../models/User");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 const register = async (req, res) => {
   const { email, password } = req.body;
@@ -25,4 +27,36 @@ const register = async (req, res) => {
   }
 };
 
-module.exports = { register };
+const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    // 1. Find the user
+    const existingUser = await User.findOne({ email });
+    if (!existingUser) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    // 2. Compare passwords
+    const isPasswordValid = await bcrypt.compare(
+      password,
+      existingUser.password
+    );
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+
+    // 3. Generate JWT
+    const token = jwt.sign({ id: existingUser._id }, "yourSecretKey", {
+      expiresIn: "1h",
+    });
+
+    // 4. Return success response with token
+    res.status(200).json({ message: "Login successful", token });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+module.exports = { register, login };
